5ade8c19455913b83aa80b2a7e630b47884c2501
vyredo <vidy.alfredo1@gmail.com>
2025-10-25 18:49:23 +0800

    add postgre permission

---
+++ postgres-init/01-permissions.sql
@@ -0,0 +1,4 @@
+ALTER DATABASE nextcloud OWNER TO nextcloud;
+ALTER SCHEMA public OWNER TO nextcloud;
+GRANT ALL PRIVILEGES ON DATABASE nextcloud TO nextcloud;
+GRANT ALL PRIVILEGES ON SCHEMA public TO nextcloud;
